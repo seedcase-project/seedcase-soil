1dfc29b8924e0c205c64ca7783144da009c9e216
Joel Ostblom <joel.ostblom@gmail.com>
2026-04-02 09:50:05 +0200

    feat: ✨ Add formatting and pretty cli printing

--- src/seedcase_soil/__init__.py
+++ src/seedcase_soil/__init__.py
@@ -1,9 +1,17 @@
 """Module containing all source code."""
 
-from .beautify_cli import CONSOLE_THEME, HELP_CONSOLE_THEME, run_without_tracebacks
+from .beautify_cli import (
+    CONSOLE_THEME,
+    HELP_CONSOLE_THEME,
+    format_param_help,
+    pretty_print,
+    run_without_tracebacks,
+)
 
 __all__ = [
     "CONSOLE_THEME",
     "HELP_CONSOLE_THEME",
+    "format_param_help",
+    "pretty_print",
     "run_without_tracebacks",
 ]
--- src/seedcase_soil/beautify_cli.py
+++ src/seedcase_soil/beautify_cli.py
@@ -1,4 +1,10 @@
+from itertools import repeat
+from typing import Optional
+
+from cyclopts.annotations import get_hint_name
+from cyclopts.help import HelpEntry
 from rich import box
+from rich import print as rprint
 from rich.console import Console
 from rich.highlighter import ReprHighlighter
 from rich.panel import Panel
@@ -27,6 +33,34 @@ HELP_CONSOLE_THEME = Theme(
         "markdown.code": "yellow not reverse",
     }
 )
+
+
+def format_param_help(entry: HelpEntry) -> str:
+    """Re-structure the parameter help into a more readable format."""
+    # Sort to put the flag first (eg `--source SOURCE` instead of the default
+    # `SOURCE --source`)
+    names = map(_add_highlight_syntax, sorted(entry.names), repeat(entry.type))
+    return f"{' '.join(names)}".strip()
+
+
+def _add_highlight_syntax(name: str, entry_type: Optional[type]) -> str:
+    """Add markup character to highlight in colors, etc where desired."""
+    formatted_name = f"[bold blue]{name}[/bold blue]"
+    if not name.startswith("-"):
+        # Matching the `dim` used by default in cyclopts for `choices` and
+        # `defaults` in the description
+        formatted_name = f"[dim]<{name}>[/dim]"
+
+        # Don't output redundant value placeholder for boolean flags
+        if get_hint_name(entry_type) == "bool":
+            formatted_name = ""
+    return formatted_name
+
+
+def pretty_print(verbose: bool, message: str) -> None:
+    """Print with prettier formatting and highlighting."""
+    if verbose:
+        rprint(message)
 
 
 def _pretty_print_error(e: Exception) -> None:

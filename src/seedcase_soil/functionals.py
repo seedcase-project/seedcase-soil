52c91df0059b916f9a0b20ea0722c7eefd88de1f
Joel Ostblom <joelostblom@users.noreply.github.com>
2026-04-13 15:59:20 +0200

    Apply suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- src/seedcase_soil/functionals.py
+++ src/seedcase_soil/functionals.py
@@ -12,15 +12,15 @@ Result = TypeVar("Result")
 def fmap(items: Iterable[In], fn: Callable[[In], Out]) -> list[Out]:
     """Apply `fn` to each element in `items`.
 
-    The difference from the build-in map is the order of the arguments
+    The difference to the build-in `map()` is the order of the arguments
     and that the output is always a list.
 
     Args:
-        items: The sequence of items.
+        items: The sequence of items, such as a list, sequence, array, or dict.
         fn: The function to apply.
 
     Returns:
-        A list with the output values after function application.
+        A list with the output values after the function is applied.
 
     """
     return list(map(fn, items))
@@ -29,15 +29,16 @@ def fmap(items: Iterable[In], fn: Callable[[In], Out]) -> list[Out]:
 def keep(items: Iterable[In], fn: Callable[[In], bool]) -> list[In]:
     """Keep elements in `items` where `fn` returns `True`.
 
-    The difference from the built-in filter is the order of the arguments,
+    The difference to the built-in `filter()` is the order of the arguments,
     and that the output is always a list.
 
     Args:
-        items: The sequence of items.
-        fn: The function that decides which items to keep.
+        items: The sequence of items, such as a list, sequence, array, or dict.
+        fn: The function with a condition that outputs a `bool`, which 
+            determines which items to keep.
 
     Returns:
-        A list of the items to keep.
+        A list of the kept items.
     """
     return list(filter(fn, items))
 
@@ -45,15 +46,13 @@ def keep(items: Iterable[In], fn: Callable[[In], bool]) -> list[In]:
 def flat_fmap(items: Iterable[In], fn: Callable[[In], Iterable[Out]]) -> list[Out]:
     """Apply `fn` to each element in `items` and flatten one level.
 
-    The difference from the built-in filter is the order of the arguments,
-    and that the output is always a list.
 
     Args:
-        items: The sequence of items.
-        fn: The function that decides which items to keep.
+        items: The sequence of items, such as a list, sequence, array, or dict.
+        fn: The function to apply to each element of `items`.
 
     Returns:
-        A list of the items to keep.
+        A list that has one level removed.
     """
     return list(chain.from_iterable(map(fn, items)))
 
@@ -63,16 +62,19 @@ def pairwise_fmap(
 ) -> list[Result]:
     """Apply `fn` to each pair of elements in `items1` and `items2`.
 
-    If `items1` has only one element, that element is repeated
-    to match the length of `items2`.
+    If `items2` has only one element, that element is repeated
+    to match the length of `items1`. Otherwise, both `item1` and
+    `item2` must be the same length. The `fn` places `item1` in
+    the first position and `item2` in the second position, e.g.
+    `fn(item1, item2)`.
 
     Args:
-        items1: The first sequence of items.
-        items2: The second sequence of items.
-        fn: The function to apply
+        items1: The sequence of items, such as a list, sequence, array, or dict.
+        items2: The sequence of items, such as a list, sequence, array, or dict.
+        fn: The function to apply on each `items1` and `items2` (in pairwise combination).
 
     Returns:
-        A list with the output values after function application.
+        A list with the output values after applying the function.
     """
     if len(items2) == 1:
         items2 = list(repeat(items2[0], len(items1)))

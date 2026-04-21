851fde6a8eba4f5faf2d3827d69f1cfb4aa150b3
Joel Ostblom <joel.ostblom@gmail.com>
2026-04-21 13:13:18 +0200

    refactor: ♻️ change how example properties are read

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,21 @@
 """Shared fixtures for tests."""
 
+import json
+
 import pytest
 
-from seedcase_soil import read_example_datapackage, write_example_datapackage
+from seedcase_soil import Example, read_properties
 
 
 @pytest.fixture
 def datapackage():
     """Return a data package dict with resources."""
-    return read_example_datapackage()
+    return read_properties(Example.simple.address)
 
 
 @pytest.fixture
-def datapackage_path(tmp_path):
+def datapackage_path(tmp_path, datapackage):
     """Create a temporary datapackage.json file and return its path as a string."""
-    return str(write_example_datapackage(tmp_path))
+    file_path = tmp_path / "datapackage.json"
+    file_path.write_text(json.dumps(datapackage))
+    return str(file_path)

73966afc7df90bf3c47f7f7f1b99b90c0fdef77f
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-05-05 16:03:42 +0200

    feat: :sparkles: add `file_tree()`

---
+++ src/seedcase_soil/file_tree.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Any
+
+import seedir as sd
+
+
+def file_tree(path: Path) -> Any:
+    """Show file tree excluding `.git` files.
+
+    Args:
+        path: The path to the package directory.
+
+    Returns:
+        A string representation of the file tree.
+    """
+    # Check if the path is a directory and exists
+    if not path.is_dir():
+        raise ValueError(f"{path} is not a directory.")
+
+    return sd.seedir(
+        path,
+        style="emoji",
+        exclude_folders=[".git"],
+        first="folders",
+        printout=False,
+    )
